36ec76fa3f037052a8f25879b6d0c672e0dc3d25
m4rshallz <s.rachaphol@gmail.com>
2025-02-12 23:04:05 +0700

    init project

---
+++ go.mod
@@ -0,0 +1,9 @@
+module hcl-slack-bot
+
+go 1.24.0
+
+require (
+	github.com/gorilla/websocket v1.5.3 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/slack-go/slack v0.16.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+	"github.com/slack-go/slack"
+)
+
+func main() {
+
+	godotenv.Load(".env")
+
+	token := os.Getenv("SLACK_AUTH_TOKEN")
+	channelID := os.Getenv("SLACK_CHANNEL_ID")
+
+	titleMessage := os.Args[1]
+	keyMsg := os.Args[2]
+
+	client := slack.New(token, slack.OptionDebug(true))
+	attachment := slack.Attachment{
+		Pretext: "Application Notification",
+		Text:    titleMessage,
+		Color:   "4af030",
+		Fields: []slack.AttachmentField{
+			{
+				// Title: keyMsg,
+				Value: time.Now().Local().Format("2006-01-02 15:04:05") + fmt.Sprintf(" - %s", keyMsg),
+			},
+		},
+	}
+
+	_, timestamp, err := client.PostMessage(
+		channelID,
+
+		slack.MsgOptionAttachments(attachment),
+	)
+
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Message sent at %s", timestamp)
+}
